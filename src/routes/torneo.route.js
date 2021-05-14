374b97ba25ee091971717d97550f0c039d666368
Mario <mmariob196@gmail.com>
2021-05-14 13:29:09 +0200

    imbecil

--- src/routes/torneo.route.js
+++ src/routes/torneo.route.js
@@ -4,16 +4,16 @@ const router = express.Router();
 const torneoController = require('../controllers/torneo.controller');
 
 // get all torneos
-router.get('/', torneoController.getParticipanteList);
+router.get('/', torneoController.getTorneosList);
 
 // post torneo nuevo
-router.post('/', torneoController.crearParticipante);
+router.post('/', torneoController.crearTorneo);
 
 // update participante 
-router.put('/:id', torneoController.actualizarParticipante);
+router.put('/:id', torneoController.actualizarTorneo);
 
 // delete participante
-router.delete('/:id',torneoController.borrarParticipante);
+router.delete('/:id',torneoController.borrarTorneo);
 
 // exporto la constante router para que pueda ser usada en los otros archivos
 module.exports = router;
